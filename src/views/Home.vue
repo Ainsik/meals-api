bc6d9fcbbb22f61acd4bf2594f575512dabfb72a
Ainsik <pio.andrzejczuk@gmail.com>
2023-04-24 18:39:43 +0200

    adds filters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,18 @@
 </template>
 
 <script setup>
-import { computed } from "@vue/reactivity";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
+import Meals from "../components/Meals.vue";
 import axiosClient from "../axiosClient.js";
 
-const meals = computed(() => store.state.meals);
+const meals = ref([]);
+
+onMounted(async () => {
+  for (let i = 0; i < 10; i++) {
+    axiosClient
+      .get(`random.php`)
+      .then(({ data }) => meals.value.push(data.meals[0]));
+  }
+});
 </script>

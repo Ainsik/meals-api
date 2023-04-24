d9b976ccd5642e19de8021c3b8f6f5128a902168
Ainsik <pio.andrzejczuk@gmail.com>
2023-04-24 15:03:11 +0200

    install axios and create connect with API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="p-8 pb-0 text-orange-500">
+    <h1 class="text-4xl font-bold mb-4">Random Meals</h1>
+  </div>
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
 import { computed } from "@vue/reactivity";
 import store from "../store";
+import axiosClient from "../axiosClient.js";
 
 const meals = computed(() => store.state.meals);
 </script>

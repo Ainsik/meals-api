081c4b622e732566c203b8beada3eb17db95854f
Ainsik <pio.andrzejczuk@gmail.com>
2023-05-02 23:16:47 +0200

    create responsive navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,22 +9,66 @@
     <div class="flex items-center gap-1">
       <router-link
         :to="{ name: 'byName' }"
-        class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+        class="hidden md:inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
       >
         Search Meals
       </router-link>
       <router-link
         :to="{ name: 'byLetter' }"
-        class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+        class="hidden md:inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
       >
         Meals By Letter
       </router-link>
       <router-link
         :to="{ name: 'ingredients' }"
-        class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+        class="hidden md:inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
       >
         Meals By Ingredients
       </router-link>
     </div>
+    <div class="md:hidden flex items-center">
+      <button @click="showMobileMenu = !showMobileMenu" class="p-5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-8 h-8 hover:w-10 hover:h-10"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+    </div>
   </header>
+  <div
+    class="md:hidden text-right bg-white"
+    :class="{ hidden: showMobileMenu }"
+  >
+    <router-link
+      :to="{ name: 'byName' }"
+      class="block px-5 py-2 hover:bg-orange-500 hover:text-lg"
+      >Search Meals</router-link
+    >
+    <router-link
+      :to="{ name: 'byLetter' }"
+      class="block px-5 py-2 hover:bg-orange-500 hover:text-lg"
+      >Meals By Letter</router-link
+    >
+    <router-link
+      :to="{ name: 'ingredients' }"
+      class="block px-5 py-2 hover:bg-orange-500 hover:text-lg"
+      >Meals By Ingredients</router-link
+    >
+  </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const showMobileMenu = ref(true);
+</script>
